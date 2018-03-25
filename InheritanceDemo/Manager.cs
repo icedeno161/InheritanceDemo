ffbb195131ff01e911c4a209e734c2b47659932c
Ivan Cedeño <icedeno@gmail.com>
2018-03-25 15:09:41 -0400

    InheritanceDemo

---
+++ InheritanceDemo/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    public class Manager : Employee
+    {
+        public bool CompanyCar { get; set; }
+
+        public Manager(string name, bool hasCar) : base(name)
+        {
+            CompanyCar = hasCar;
+        }
+
+        public override void TakeVacation()
+        {
+            vacationDays += 15;
+        }
+
+        public override string ToString()
+        {
+            return $"[Manager: Name = {Name} Company Car = {CompanyCar} Vacaction Days = {vacationDays}";
+        }
+    }
+}
---
+++ InheritanceDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bob = new Worker("Bob", 35.00);
+            var joe = new Manager("Joe", true);
+            var sally = new Worker("Sally", 27.50);
+
+            var employees = new List<Employee>()
+            {
+                bob,
+                joe,
+                sally
+            };
+
+            foreach (var item in employees)
+            {
+                item.TakeVacation();
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
